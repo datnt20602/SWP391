760ed364c363162a089a313c400cae28b732198f
davidMinhDn <134630652+davidMinhDn@users.noreply.github.com>
2023-06-09 14:50:50 +0700

    minhdn
    tao object Product va DAOProduct

---
+++ src/main/java/Model/Product.java
@@ -0,0 +1,88 @@
+package Model;
+
+public class Product {
+    private int product_id;
+    private String product_name;
+    private String image;
+    private String manufacturing_date;
+    private double price;
+    private String type_name;
+    private String category;
+    private String description;
+    private int quantity;
+
+    public Product() {
+    }
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getManufacturing_date() {
+        return manufacturing_date;
+    }
+
+    public void setManufacturing_date(String manufacturing_date) {
+        this.manufacturing_date = manufacturing_date;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getType_name() {
+        return type_name;
+    }
+
+    public void setType_name(String type_name) {
+        this.type_name = type_name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
