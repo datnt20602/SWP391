e901e253d734e41e78adb18898f5127793cc9c15
davidMinhDn <134630652+davidMinhDn@users.noreply.github.com>
2023-06-11 15:24:59 +0700

    minhdnhe161535
    object product

---
+++ src/main/java/Model/Product.java
@@ -0,0 +1,79 @@
+package Model;
+
+public class Product {
+    private int product_id;
+    private String product_name;
+    private String date_of_manufacture;
+    private String category_name;
+    private double price;
+    private String image;
+    private String describe;
+    private int quantity;
+
+    public Product() {
+    }
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public String getDate_of_manufacture() {
+        return date_of_manufacture;
+    }
+
+    public void setDate_of_manufacture(String date_of_manufacture) {
+        this.date_of_manufacture = date_of_manufacture;
+    }
+
+    public String getCategory_name() {
+        return category_name;
+    }
+
+    public void setCategory_name(String category_name) {
+        this.category_name = category_name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
