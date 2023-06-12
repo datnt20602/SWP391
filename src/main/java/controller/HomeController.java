c20588cdbc25253f04cb2872763ba657f54a5c9f
datnt20602 <qwertyy.no01@gmail.com>
2023-06-12 20:56:27 +0700

    change /home

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -6,7 +6,7 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
-@WebServlet(name = "HomeController", value = "/home")
+@WebServlet(name = "HomeController", value = {"/home", "/"})
 public class HomeController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
