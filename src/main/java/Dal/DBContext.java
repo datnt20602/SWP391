e51a11341fbf4109f2e24f4a16e7bd23260dfbbd
davidMinhDn <134630652+davidMinhDn@users.noreply.github.com>
2023-06-23 14:37:24 +0700

    minhdn
    create DAO object

--- src/main/java/Dal/DBContext.java
+++ src/main/java/Dal/DBContext.java
@@ -1,9 +1,7 @@
 package Dal;
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -26,6 +24,18 @@ import java.util.logging.Logger;
             } catch (SQLException ex) {
                 System.out.println("Connect Fail: " + ex.getMessage());
             }
+        }
+        public ResultSet getData(String sql){
+            ResultSet rs=null;
+            try {
+                Statement state = connection.createStatement(
+                        ResultSet.TYPE_SCROLL_SENSITIVE,
+                        ResultSet.CONCUR_UPDATABLE);
+                rs=state.executeQuery(sql);
+            } catch (SQLException ex) {
+                Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return rs;
         }
 
         public static void main(String[] args) {
