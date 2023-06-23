7ea95f06bba5ada3de0fb634e728a38059c6eba8
davidMinhDn <134630652+davidMinhDn@users.noreply.github.com>
2023-06-23 12:10:59 +0700

    minhdn
    fit xung dot

--- src/main/java/Dal/DBContext.java
+++ src/main/java/Dal/DBContext.java
@@ -17,7 +17,7 @@ import java.util.logging.Logger;
         public DBContext() {
             try {
                 String user = "root";
-                String pass = "dat2062002";
+                String pass = "minh2002";
                 String url = "jdbc:mysql://localhost:3306/drink_online_shop1";
                 Class.forName("com.mysql.cj.jdbc.Driver");
                 connection = DriverManager.getConnection(url, user, pass);
--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -11,13 +11,13 @@ public class HomeController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
-        if (session.getAttribute("isAdmin") != null) {
-            request.getRequestDispatcher("template/front-end/admin-home.jsp").forward(request, response);
-        } else if (session.getAttribute("isStaff") != null) {
-            request.getRequestDispatcher("template/front-end/staff-home.jsp").forward(request, response);
-        } else {
-            request.getRequestDispatcher("template/front-end/home.jsp").forward(request, response);
-        }
+//        if (session.getAttribute("isAdmin") != null) {
+//            request.getRequestDispatcher("template/front-end/admin-home.jsp").forward(request, response);
+//        } else if (session.getAttribute("isStaff") != null) {
+//            request.getRequestDispatcher("template/front-end/staff-home.jsp").forward(request, response);
+//        } else {
+//            request.getRequestDispatcher("template/front-end/home.jsp").forward(request, response);
+//        }
 
     }
 
